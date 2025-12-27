ee2f68c7bf2b09436b4d9d75655c754102922289
unknown <pawanb532@gmail.com>
2025-12-28 00:45:17 +0530

    print different way

---
+++ print.py
@@ -0,0 +1,7 @@
+a = 2
+b = 4
+print ("The value a is ", a, "The value of b is",b)
+print (" The value of a and b is", a+b)
+print ("value of a is {} value of b is {}". format (a, b))
+print (f"value of a is {a} value of b is {b}")
+print ("value of a is %d value of b is %d" % (a,b))
